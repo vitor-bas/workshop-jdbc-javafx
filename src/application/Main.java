513ed859b01dbdfb79c993753561dab137f2d5ca
vitor-bas <vitor212009@hotmail.com>
2020-11-22 21:00:23 -0300

    About View

--- src/application/Main.java
+++ src/application/Main.java
@@ -11,12 +11,15 @@ import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.BorderPane;
 
 public class Main extends Application {
+	
+	private static Scene mainScene;
+	
 	@Override
 	public void start(Stage primaryStage) {
 		try {
 			FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/MainView.fxml"));
 			ScrollPane scrollPane = loader.load();
-			Scene mainScene = new Scene(scrollPane);
+			mainScene = new Scene(scrollPane);
 			
 			scrollPane.setFitToHeight(true);
 			scrollPane.setFitToWidth(true);
@@ -27,6 +30,10 @@ public class Main extends Application {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+	}
+	
+	public static Scene getMainScene() {
+		return mainScene;
 	}
 
 	public static void main(String[] args) {
